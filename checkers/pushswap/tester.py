98c9bf39786b16ea550363ed77d432a013171cb6
Maxime <114405471+3andan4@users.noreply.github.com>
2022-11-23 15:45:49 +0100

    Ultra moche mais normalement il marche

--- checkers/pushswap/tester.py
+++ checkers/pushswap/tester.py
@@ -1,78 +1,99 @@
-#This one will work :hope:
+#!/usr/bin/python3
 
 import sys
 
-file_opened = open("./"+sys.argv[1], 'r')
-stack_a = file_opened.read().replace('\n', '').replace('\0', '').split(' ')
-file_opened = open("./"+sys.argv[2], 'r')
-operations = file_opened.read().replace('\n', '').replace('\0', '').split(' ')
-len_a = len(stack_a)
-stack_b = []
+def is_in_order(list: list):
+    for i in range(0, len(list) - 1):
+        if list[i] > list[i + 1]:
+            return False
+    return True
 
-if len_a == 1:
-    print("\033[92mPushswap OK\033[0m")
+def swap_firsts(list: list):
+    tmp = list[0]
+    list[0] = list[1]
+    list[1] = tmp
+    return list
 
-def is_sorted():
-    global stack_a, len_a
-    if len(stack_a) != len_a:
-        return (False)
-    for i in range(1, len_a):
-        if int(stack_a[i]) < int(stack_a[i - 1]):
-            return (False)
-    return (True)
+def rotate(list: list):
+    list.append(list.pop(0))
+    return list
 
-def sa():
-    global stack_a
-    if len(stack_a) >= 2:
-        stack_a[0], stack_a[1] = stack_a[1], stack_a[0]
-def sb():
-    global stack_b
-    if len(stack_b) >= 2:
-        stack_b[0], stack_b[1] = stack_b[1], stack_b[0]
-def pa():
-    global stack_a, stack_b
-    if len(stack_b) > 0:
-        stack_a.insert(0, stack_b.pop(0))
-def pb():
-    global stack_a, stack_b
-    if len(stack_a) > 0:
-        stack_b.insert(0, stack_a.pop(0))
-def ra():
-    global stack_a
-    if len(stack_a) > 0:
-        stack_a.append(stack_a.pop(0))
-def rb():
-    global stack_b
-    if len(stack_b) > 0:
-        stack_b.append(stack_b.pop(0))
-def rra():
-    global stack_a
-    if len(stack_a) > 0:
-        stack_a.insert(0, stack_a.pop(len(stack_a) - 1))
-def rrb():
-    global stack_b
-    if len(stack_b) > 0:
-        stack_b.insert(0, stack_b.pop(len(stack_b) - 1))
 
-for i in range(len(operations)):
-    if operations[i] == "sa":
-        sa()
-    if operations[i] == "sb":
-        sb()
-    if operations[i] == "pa":
-        pa()
-    if operations[i] == "pb":
-        pb()
-    if operations[i] == "ra":
-        ra()
-    if operations[i] == "rb":
-        rb()
-    if operations[i] == "rra":
-        rra()
-    if operations[i] == "rrb":
-        rrb()
+def reverse_rotate(list: list):
+    list.insert(0, list.pop())
+    return list
 
-if is_sorted():
-    print("\033[92mPushswap OK\033[0m")
-else:
-    print("\033[91mPushswap KO\033[0m")
+
+def push(list_a: list, list_b: list):
+    list_b.insert(0, list_a.pop(0))
+    return [list_a, list_b]
+
+
+def execute_instruction(instructions, number_list):
+    list_b = []
+    print(f"Executing {len(instructions.split())} instructions on The List")
+    for instruction in instructions.split(' '):
+        if instruction == 'ra':
+            number_list = rotate(number_list)
+        elif instruction == 'rra':
+            number_list = reverse_rotate(number_list)
+        elif instruction == 'pb':
+            r = push(number_list, list_b)
+            number_list = r[0]
+            list_b = r[1]
+        elif instruction == 'pa':
+            r = push(list_b, number_list)
+            number_list = r[1]
+            list_b = r[0]
+        elif instruction == 'sa':
+            number_list = swap_firsts(number_list)
+        elif instruction == 'sb':
+            list_b = swap_firsts(list_b)
+        elif instruction == 'sc':
+            number_list = swap_firsts(number_list)
+            list_b = swap_firsts(list_b)
+        elif instruction == 'rr':
+            number_list = rotate(number_list)
+            list_b = rotate(list_b)
+        elif instruction == 'rrr':
+            number_list = reverse_rotate(number_list)
+            list_b = reverse_rotate(list_b)
+        else:
+            print("Invalid instruction")
+            sys.exit(1)
+    if (len(list_b) != 0):
+        print("Error: list b not empty")
+        # Print a colored KO
+        print("\033[91mKO\033[0m")
+        sys.exit(1)
+    if (is_in_order(number_list)):
+        print("The List is in order")
+        print("\033[92mOK\033[0m")
+        sys.exit(0)
+    else:
+        print("The List is not in order")
+        print("\033[91mKO\033[0m")
+        sys.exit(1)
+
+
+def file_to_int_list(file_name):
+    list = []
+    with open(file_name, 'r') as f:
+        for num in f.read().split():
+            list.append(int(num))
+    return list
+
+
+def file_to_string(file_name):
+    with open(file_name, 'r') as f:
+        return f.read().strip()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(
+            "Usage: ./push_swap.py [list of numbers - file] [list of instructions - file]")
+        exit(1)
+    number_list = file_to_int_list(sys.argv[1])
+    instructions = file_to_string(sys.argv[2])
+    execute_instruction(instructions, number_list)
