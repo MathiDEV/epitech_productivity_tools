eb693ea66dba67eaf9cc5f8e08994c268e2bb6e8
Maxime <114405471+3andan4@users.noreply.github.com>
2022-11-24 15:37:17 +0100

    Delete gen_nb.py

--- checkers/pushswap/gen_nb.py
+++
@@ -1,9 +0,0 @@
-import random
-import sys
-if (len(sys.argv) != 4):
-    exit(84)
-for i in range(int(sys.argv[1])):
-    print(random.randint(int(sys.argv[2]), int(sys.argv[3])), end="")
-    if (i != int(sys.argv[1]) - 1):
-        print(" ", end="")
-print("")
